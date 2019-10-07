c36170103e6740e382379636999c05cb10e2602a
Koruja <gustavowl@lcc.ufrn.br>
2019-10-07 10:36:57 -0300

    Verifies if result is sorted

--- Chapter07/quicksort_rec.c
+++ Chapter07/quicksort_rec.c
@@ -31,8 +31,33 @@ void quicksort(int* a, int l, int r) {
 	}
 }
 
+int is_sorted(int* a, int size) {
+	for (int i = 1; i < size; i++) {
+		if (a[i-1] > a[i])
+			return 0;
+	}
+
+	return 1;
+}
+
+void print_array(int*a, int size) {
+	for (int i = 0; i < size; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+}
+
 int main() {
-	int array[] = {5, 2, 2, 3, 6, 7, 10, 92};
-	quicksort(array, 0, sizeof(array)/sizeof(int) - 1);
+	int array[] = {2091, 1743, 1065, 1800, 1858, 73, 1945, 2061, 290, 917, 712, 2112, 1480, 1030};
+	int size = sizeof(array)/sizeof(int);
+
+	printf("Input array: ");
+	print_array(array, size);
+
+	quicksort(array, 0, size - 1);
+
+	printf("Output array: ");
+	print_array(array, size);
+	printf("Sorted: %s\n", is_sorted(array, size) ? "true" : "false");
+
 	return 0;
 }
