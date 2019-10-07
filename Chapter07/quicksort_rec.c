ca0d032ad94b5e1f5b8f3e865147321ac813cb86
Koruja <gustavowl@lcc.ufrn.br>
2019-10-07 10:19:59 -0300

    Implements partition

--- Chapter07/quicksort_rec.c
+++ Chapter07/quicksort_rec.c
@@ -2,7 +2,24 @@
 
 //array, left, right
 int partition(int* a, int l, int r) {
-	return -1;
+	int k = r; //key index
+	int i = l;
+	int swap;
+
+	while (i < k) {
+		if (a[i] <= a[k])
+			i++;
+		else {
+			//swap
+			swap = a[k];
+			a[k] = a[i];
+			a[i] = a[k - 1];
+			a[k - 1] = swap;
+			k--;
+		}
+	}
+
+	return k;
 }
 
 //array, left, right
@@ -15,7 +32,7 @@ void quicksort(int* a, int l, int r) {
 }
 
 int main() {
-	int array[] = {5, 2, 3, 6, 7, 10, 92};
+	int array[] = {5, 2, 2, 3, 6, 7, 10, 92};
 	quicksort(array, 0, sizeof(array)/sizeof(int) - 1);
 	return 0;
 }
